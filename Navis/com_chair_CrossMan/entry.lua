e1b033ae86fe76c9257d7298aae2eb4be3ab1b16
Taireru Benaru <38292283+chairdev@users.noreply.github.com>
2025-02-27 15:33:40 +0100

    remove unused assets for CrossMan

---
+++ Navis/com_chair_CrossMan/entry.lua
@@ -0,0 +1,60 @@
+function package_init(package)
+    package:declare_package_id("com.chair.CrossMan")
+    package:set_special_description("CrossMan.ExE with CrossLink!")
+    package:set_speed(2.0)
+    package:set_attack(1)
+    package:set_charged_attack(10)
+    package:set_icon_texture(Engine.load_texture(_folderpath.."icon.png"))
+    package:set_preview_texture(Engine.load_texture(_folderpath.."preview.png"))
+    package:set_overworld_animation_path(_folderpath.."overworld.animation")
+    package:set_overworld_texture_path(_folderpath.."overworld.png")
+    package:set_mugshot_texture_path(_folderpath.."mug.png")
+    package:set_mugshot_animation_path(_folderpath.."mug.animation")
+    package:set_emotions_texture_path(_folderpath.."emotions.png")
+end
+
+function player_init(player)
+    player:set_name("CrossMan")
+    player:set_health(1000)
+    player:set_element(Element.None)
+    player:set_height(38.0)
+
+    local base_texture = Engine.load_texture(_folderpath.."battle.png")
+    local base_animation_path = _folderpath.."battle.animation"
+    local base_charge_color = Color.new(57, 198, 243, 255)
+
+    player:set_animation(base_animation_path)
+    player:set_texture(base_texture)
+    player:set_fully_charged_color(base_charge_color)
+    player:set_charge_position(0, -20)
+
+    --Charge Shots
+    local cross_gun = include("Chips/Neutral/CrossGun/entry.lua")
+
+    --Special Attacks
+    local cross_shield = include("Chips/Neutral/CrossShield/entry.lua")
+
+    --NCPs
+    local super_armor = Battle.DefenseRule.new(813, DefenseOrder.CollisionOnly)
+	super_armor.filter_statuses_func = function(statuses)
+		statuses.flags = statuses.flags & ~Hit.Flinch
+		return statuses
+	end
+
+    player.normal_attack_func = function()
+        return Battle.Buster.new(player, false, player:get_attack_level())
+    end
+
+    player.charged_attack_func = function()
+        local props = Battle.CardProperties:new()
+        props.damage = player:get_attack_level() * 10
+        return cross_gun.card_create_action(player, props)
+        --return Battle.Buster.new(player, true, player:get_attack_level() * 10)
+    end
+
+    player.special_attack_func = function()
+        local props = Battle.CardProperties:new()
+        props.damage = 20 + ((player:get_attack_level()) * 10)
+        return cross_shield.card_create_action(player, props)
+    end
+end
